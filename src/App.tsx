96732345f2a8611bdd0982c36f3f9c05a7da1dbd
Michael Gomeh <gomehmichael@gmail.com>
2025-03-20 19:07:29 +0200

    add dayjs, init app main components and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import React from 'react';
+import dayjs from 'dayjs';
 import './App.css';
 
-function App() {
-	return <div>Hello Template</div>;
-}
+const App: React.FC = () => {
+	const hours = Array.from({ length: 16 }, (_, i) => {
+		return dayjs()
+			.startOf('day')
+			.add(i + 6, 'hour')
+			.format('h:mm A');
+	});
+
+	return (
+		<div className="app">
+			<h1>Hourly</h1>
+			<div className="hour-column">
+				{hours.map((hour) => (
+					<div key={hour} className="hour-block">
+						<div className="hour-label">{hour}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default App;
